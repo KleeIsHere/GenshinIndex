826c62baad6fa421bb177c552e79597b91ff88a9
Klee <kleegenshin1o1@gmail.com>
2021-10-23 20:39:06 -0600

    Started writing out the structure for Itto's character class with skills.

--- Characters/Itto/IttoCharacter.py
+++ Characters/Itto/IttoCharacter.py
@@ -1,22 +1,26 @@
 import pandas
 
-class Kazuha:
+class Itto:
 
-    #making 
-    baseATK = 297
+    # Itto's base stats (later we will pull these based off his lvl from a table) LVL 90 right now
+    baseATK = 191
+    baseDEF = 930
     attackBonus = 0
     critDamage = 0.5
-    critRate = 0.05
+    critRate = 0.242
     elementalBonus = 0
-    elementalMastery = 115.2
-    type = "Anemo"
+    elementalMastery = 0
+    NormalAttackCounter = 1
+    type = "Geo"
 
-    #
-    def __init__(self, LVL, T1, T2, T3):
+    # constructor (used to initialize and instance of the Itto class)
+    def __init__(self, LVL, T1, T2, T3, Con):
         self.LVL = LVL
         self.T1 = T1
         self.T2 = T2
         self.T3 = T3
+        self.Con = Con
+        
 
     def create_attack_string():
         x = 0
@@ -24,3 +28,25 @@ class Kazuha:
         # after identifying that we are in fact doing a normal attack then we need to determine the sequence of attacks
         # 3 Talents [Normal, Elemental, Burst] then we can number hits afterwards. also we can use A for all hits
         # pass in the atk sequnce string and then return multipliers with atk type classification
+
+
+    # Normal Attack Cycle
+    # Function that will increase the normal attack counter and just pull the correct multiplier for it
+    
+    # Specific Normal Attack
+    # Pass a number to specify which normal attack that we want
+
+    # Charged Attacks
+
+    # Plunging Attacks
+
+    # Burst (just buffs him and changes his dmg type)
+    # This will pass the buffs
+
+    # Passive Talents
+    # Arataki Ichiban - Gets an Atk Speed increase 10% on charged attacks
+    # Bloodline - Arataki Kasegiri DMG is increased by 35% of DEF
+    # Woodchuck - omit
+    # Warrior's Burden - E skill or sprint activation will not reset his attack skill combo
+
+    # 
